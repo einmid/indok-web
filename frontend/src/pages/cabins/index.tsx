12d4ca24c46c012350153c45a494800f9a38f7de
Mathias Raa <skraagen@hotmail.com>
2022-04-30 15:52:32 +0200

    cabins frontpage upgrades

--- frontend/src/pages/cabins/index.tsx
+++ frontend/src/pages/cabins/index.tsx
@@ -1,8 +1,10 @@
 import CabinPrices from "@components/pages/cabins/CabinPrices";
+import CabinsDetailsSection from "@components/pages/cabins/CabinsDetailsSection";
+import CabinsHero from "@components/pages/cabins/CabinsHero";
+import CabinsInfoSection from "@components/pages/cabins/CabinsInfoSection";
 import ContactCabinBoard from "@components/pages/cabins/ContactCabinBoard";
 import FAQ from "@components/pages/cabins/Documents/FAQ";
-import Hero from "@components/pages/cabins/Hero";
-import { cabinImages, outsideImages } from "@components/pages/cabins/ImageSlider/imageData";
+import { outsideImages } from "@components/pages/cabins/ImageSlider/imageData";
 import ImageSlider from "@components/pages/cabins/ImageSlider/ImageSlider";
 import { Box, Container, Divider, Grid, styled, Typography } from "@mui/material";
 import Image from "next/image";
@@ -24,33 +26,6 @@ const RootStyle = styled("div")(({ theme }) => ({
 }));
 
 const CabinsPage: NextPageWithLayout = () => {
-  const facilitiesData = [
-    {
-      icon: <Image alt="" src="/img/undraw_home.svg" width={100} height={100} />,
-      text: "Varmekabler",
-    },
-    {
-      icon: <Image alt="" src="/img/undraw_electricity.svg" width={100} height={100} />,
-      text: "Innlagt strøm",
-    },
-    {
-      icon: <Image alt="" src="/img/undraw_speaker.svg" width={100} height={100} />,
-      text: "Høyttaleranlegg",
-    },
-    {
-      icon: <Image alt="" src="/img/undraw_bed.svg" width={100} height={100} />,
-      text: "18 soveplasser",
-    },
-    {
-      icon: <Image alt="" src="/img/undraw_cooking.svg" width={100} height={100} />,
-      text: "Kjøkken",
-    },
-    {
-      icon: <Image alt="" src="/img/undraw_cabin.svg" width={100} height={100} />,
-      text: "Badstue",
-    },
-  ];
-
   const transportData = [
     {
       icon: <Image alt="" src="/img/undraw_bus_stop.svg" width={200} height={200} />,
@@ -91,71 +66,12 @@ const CabinsPage: NextPageWithLayout = () => {
 
   return (
     <RootStyle>
-      <Hero />
+      <CabinsHero />
+      <CabinsInfoSection />
+      <CabinsDetailsSection />
       <Container>
         <Box my={5} id="anchorBox">
           <Grid container alignItems="center" spacing={10} direction="column">
-            <Grid item>
-              <Grid container alignItems="center" spacing={10}>
-                <Grid xs={12} sm={6} item>
-                  <Box textAlign="center">
-                    <Typography variant="h3">Fasiliteter</Typography>
-                  </Box>
-                  <Divider />
-                  <Box m={3}>
-                    <Grid container spacing={4} justifyContent="center">
-                      {facilitiesData.map((facility) => (
-                        <Grid item md={4} sm={6} xs={6} key={facility.text}>
-                          <Box textAlign="center">
-                            {facility.icon}
-                            <Typography variant={"body2"}>{facility.text}</Typography>
-                          </Box>
-                        </Grid>
-                      ))}
-                    </Grid>
-                  </Box>
-                </Grid>
-                <Grid xs={12} sm={6} item>
-                  <Typography variant="h3">Indøkhyttene - Oksen og Bjørnen</Typography>
-                  <Divider component="br" />
-                  <Typography variant="body2">
-                    De to identiske nabohyttene ligger idyllisk til, kun et steinkast unna Stølen alpinsenter i Oppdal.
-                    Hyttene har flere bruksområder; alt fra strategiske samlinger og egne arrangementer til sosiale,
-                    spontane venneturer. Det er en gyllen mulighet til å få en liten pause fra det travle bylivet.
-                    Indøks egne Hyttestyre arrangerer flere forskjellige turer i løpet av året. Dette er en flott
-                    mulighet til både å bli kjent med hyttene, området rundt hyttene, og å bli kjent med andre indøkere
-                    på tvers av klassetrinnene. Hyttestyret har det daglige ansvaret for drift og utbedring av
-                    Indøkhyttene, organisering av utleie og felles hytteturer. Du kan lese mer om Hyttestyret
-                    <Link href="/about/organizations/hyttestyret"> her.</Link>
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item>
-              <Grid container alignItems="center" spacing={10} direction="row">
-                <Grid item xs={12} sm={6}>
-                  <Typography variant="h3">Hyttenes standard</Typography>
-                  <Divider component="br" />
-                  <Typography variant="body2">
-                    Med sine to etasjer, er Bjørnen og Oksen estimert til å romme 18 personer per hytte. Den generelle
-                    standarden er tilnærmet lik et vanlig bolighus. Begge hyttene har innlagt strøm og vann. I første
-                    etasje finner du to bad, hvorav ett med badstue, og tre soverom med tre til fire sengeplasser per
-                    rom. I andre etasje ligger stue, kjøkken og et fjerde soverom med sengeplass til tre. Dette gir
-                    totalt fjorten sengeplasser på hver hytte og ekstramadrasser til de resterende 4 det er estimert
-                    med. Dyner og puter til 18 gjester ligger tilgjengelig, men laken og sengetøy må medbringes.
-                    Kjøkkenet er utstyrt med det mest nødvendige av hvitevarer, i tillegg til kaffetrakter, vannkoker og
-                    vaffeljern m.m. Basiskrydder og olje til steking skal også være tilgjengelig. På hyttene ligger det
-                    et bredt utvalg brettspill, samt kortstokker. I stua står det anlegg med AUX-kabel.
-                  </Typography>
-                </Grid>
-
-                <Grid item container xs={12} sm={6} justifyContent="center" alignContent="center">
-                  <Box width="90%">
-                    <ImageSlider imageData={cabinImages} displayLabelText={false} />
-                  </Box>
-                </Grid>
-              </Grid>
-            </Grid>
             <Grid item>
               <Grid container spacing={5} direction="column">
                 <Grid item>
